b141899422cade4b6a577d9209e52086fe70a502
unknown <flp_petkovski@yahoo.com>
2020-09-17 10:10:09 +0200

    last dev commit

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const productRouter = require("./routers/product")
 
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT
 
 app.use(express.json());
 app.use(userRouter)
--- src/models/user.js
+++ src/models/user.js
@@ -57,7 +57,7 @@ userSchema.methods.generateAuthToken = async function () {
   const user = this;
   const token = jwt.sign({
     _id: user.id.toString()
-  }, "thisissupersecret123");
+  }, process.env.JWT_SECRET);
   user.tokens = user.tokens.concat({
     token
   });
