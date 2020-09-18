ba5b52fba149e5e3f15ff04348170703c801013e
unknown <flp_petkovski@yahoo.com>
2020-09-18 10:14:20 +0200

    added cors

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
-require("./db/mongoose")
+require("./db/mongoose");
+const cors = require("cors");
 
 const express = require("express");
 
-const userRouter = require("./routers/user")
-const productRouter = require("./routers/product")
+const userRouter = require("./routers/user");
+const productRouter = require("./routers/product");
 
 const app = express();
 
-const port = process.env.PORT
+const port = process.env.PORT;
+
+app.use(cors({ origin: true }));
 
 app.use(express.json());
-app.use(userRouter)
-app.use(productRouter)
-
+app.use(userRouter);
+app.use(productRouter);
 
 app.listen(port, () => {
   console.log(`port is up on ${port}`);
