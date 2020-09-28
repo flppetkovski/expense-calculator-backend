894c802cadf9369833811c2264d56dcb71d2c6e8
unknown <flp_petkovski@yahoo.com>
2020-09-28 19:12:18 +0200

    fixed CORS

--- src/index.js
+++ src/index.js
@@ -10,7 +10,12 @@ const app = express();
 
 const port = process.env.PORT;
 
-app.use(cors({ origin: "*" }));
+app.use((req, res, next) => {
+  res.append("Access-Control-Allow-Origin", ["*", "localhost:3000", "null"]);
+  res.append("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.append("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 
 app.use(express.json());
 app.use(userRouter);
