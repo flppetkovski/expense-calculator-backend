8cccf4b7a69163758aba8f72e51843d6746040fe
unknown <flp_petkovski@yahoo.com>
2020-09-28 18:25:49 +0200

    fixed CORS

--- src/index.js
+++ src/index.js
@@ -3,19 +3,6 @@ const cors = require("cors");
 
 const express = require("express");
 
-app.use(function (req, res, next) {
-  let whitelist = ["localhost:4000", "localhost:3000", "anydomain.com", "null"], ;
-  let host = req.get("host");
-
-  whitelist.forEach(function (val, key) {
-    if (host.indexOf(val) > -1) {
-      res.setHeader("Access-Control-Allow-Origin", host);
-    }
-  });
-
-  next();
-});
-
 const userRouter = require("./routers/user");
 const productRouter = require("./routers/product");
 
