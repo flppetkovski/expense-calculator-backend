8dfbf5dacfe7839ed6dd659dbc9336e59781a31e
unknown <flp_petkovski@yahoo.com>
2020-09-28 19:23:04 +0200

    fixed CORS

--- src/index.js
+++ src/index.js
@@ -9,7 +9,18 @@ const productRouter = require("./routers/product");
 const app = express();
 
 const port = process.env.PORT;
-app.use(cors({ origin: "*" }));
+// app.use(cors({ origin: "*" }));
+app.use((req, res, next) => {
+  res.append("Access-Control-Allow-Origin", [
+    "*",
+    "null",
+    "http://localhost:3000",
+    "http://localhost:3000",
+  ]);
+  res.append("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.append("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 
 app.use(express.json());
 app.use(userRouter);
