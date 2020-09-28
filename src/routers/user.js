707fd292bccf2bf721491685293c16713c340dc2
unknown <flp_petkovski@yahoo.com>
2020-09-28 20:15:08 +0200

    cors for netlify

--- src/routers/user.js
+++ src/routers/user.js
@@ -116,8 +116,6 @@ const upload = multer({
 });
 
 router.post(
-  res.header("Access-Control-Allow-Origin", "*"),
-  "/users/me/avatar",
   auth,
   upload.single("avatar"),
   async (req, res) => {
